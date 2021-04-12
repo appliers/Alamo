428ad7c377bf806b77e224478aa526f766edc08a
xkwm1 <xkwm1yt@gmail.com>
2021-04-12 11:47:11 -0700

    got rid of the commented code

--- scams.py
+++ scams.py
@@ -4,7 +4,6 @@ import logging
 import random
 import string
 import json
-# import grequests
 
 from colored import fore, back, style
 
@@ -26,71 +25,22 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 request = requests.get(url, headers=headers)
 if request.status_code == 400:
     print('This website does not exist bruh')
-# url = urlInput
-# print(url)
-
-
-
-
-# print(fore.GREEN + "What is the scammer's URL???: ", end='')
-# urlInput = input()
-# url = 'https://'+urlInput
-# print(url)
-
-# urlInput = "{url}"
 
 # ==========================================================================================================
 
-# usernameInput = input("What is the log in information?: ")
-# print(usernameInput)
 print(Fore.RED + "[SP]" + Fore.CYAN + ' What is the login Form Data?: ' + Style.RESET_ALL, end="")
 usernameInput = input()
-# print(usernameInput)
 
 
 # ==========================================================================================================
 
-# passwordInput = input("What is the password information?: ")
 print(Fore.RED + "[SP]" + Fore.CYAN + ' What is the password Form Data?: ' + Style.RESET_ALL, end="")
 passwordInput = input()
-# print(passwordInput)
-# print(passwordInput)
 
 # ==========================================================================================================
 
 requestInput = int(input(f"{Fore.RED}[SP] {Fore.CYAN}How many requests do you want to make?: {Style.RESET_ALL}"))
-# print(Fore.RED + "[SP]" + Fore.CYAN + ' How many requests do you want to make?: ' + Style.RESET_ALL, end="".isdecimal())
 print(requestInput)
-
-#def notnumber(answer):
-#	answer = print(Fore.ORANGE + "[ERROR]" + Fore.CYAN + 'This is not a number')
-#	return answer
-
-# requestInput = input(),
-#if isdigit() == False:
-#    print('Please only enter a number')
-
-
-# print(requestInput)
-
-# requestInput = input("How many requests do you want to send to the website?: ")
-# print(requestInput)
-# rangeInput = range(requestInput)
-
-
-# password = ''.join(random.choice(chars) for i in range(8))
-
-# requests.post(url, allow_redirects=False, data={
-#	'{usernameInput}': username,
-# 	'{passwordInput}': password
-# })
-
-#for i in range(amt):
-#    ending = ""
-#    for i in range(random.randint(19,19)):
-#        ending += random.choice(chars)
-#    url = (main+ending)
-#    print(url)
 
 name_extra = ''.join(random.choice(string.digits))
 
@@ -110,19 +60,6 @@ for i in range(requestInput):
 		f"{usernameInput}": username,
 		f"{passwordInput}": password
 	})
-    #for i in range(random.randint(2,2)):
-        # print(f"Sending {style.BOLD}{requestInput}{Style.RESET_ALL} requests right now, please wait...")
     print(f"Sending {style.BOLD}1{Style.RESET_ALL} requests with the Username {style.UNDERLINED}{username} and Password {style.UNDERLINED}{password}")
 
 print(f"{Fore.RED}[SP] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
-
-# p.map(requests.post, urls)
-
-# rs = (grequests.get(u) for u in urls)
-# grequests.map(rs)
-
-# Sending ' + Style.BOLD + requestInput + Style.RESET_ALL + ' requests with the Username %s and Password %s' % (username, password
-
-# print(f"Sending {style.BOLD}{requestInput}{Style.RESET_ALL} requests with the Username {username} and Password {password}")
-
-# print('Sending ' + Style.BOLD + requestInput + Style.RESET_ALL + ' requests with the Username %s and Password %s' % (username, password))

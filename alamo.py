4ce866774ac45f8d5030c146090c6b3e9c75f7ca
xkwm1 <xkwm1yt@gmail.com>
2021-04-13 11:53:46 -0700

    it broke when i reorganized the code lol so i fixed it

--- alamo.py
+++ alamo.py
@@ -75,7 +75,7 @@ if optionInput == "1":
     import ctypes
     ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Login...")
 
-    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] What is the login Form Data?: {Style.RESET_ALL}", end="") f"[{Fore.RED}Alamo{Style.RESET_ALL}] What is the login Form Data?: {Style.RESET_ALL}", end=""
+    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] {Fore.CYAN}What is the login Form Data?: {Style.RESET_ALL}", end="")
     usernameInput = input()
 
 
@@ -85,7 +85,7 @@ if optionInput == "1":
     ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Password...")
 
 
-    print("[{Fore.RED}Alamo{Style.RESET_ALL}] What is the password Form Data?: {Style.RESET_ALL}", end="")
+    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] {Fore.CYAN}What is the password Form Data?: {Style.RESET_ALL}", end="")
     passwordInput = input()
 
     # ==========================================================================================================
@@ -97,7 +97,7 @@ if optionInput == "1":
     #requestInput = int(input(f"{Fore.RED}[SP] {Fore.CYAN}How many requests do you want to make?: {Style.RESET_ALL}"))
     #print(requestInput)
 
-    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] How many requests do you want to send?: {Style.RESET_ALL}", end="")
+    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] {Fore.CYAN}How many requests do you want to send?: {Style.RESET_ALL}", end="")
 
     request2Input = input()
 
@@ -120,7 +120,7 @@ if optionInput == "1":
     ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Proxy...")
 
 
-    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] What you like to use a Proxy?: {Style.RESET_ALL}", end="")
+    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] {Fore.CYAN}What you like to use a Proxy?: {Style.RESET_ALL}", end="")
     proxyInput = input()
 
     if "yes" in proxyInput or "no" in proxyInput:

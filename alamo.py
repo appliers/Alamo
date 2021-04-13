b0a655d7d14cdc2ebacb4f8c613201c0bb0ae2a8
xkwm1 <xkwm1yt@gmail.com>
2021-04-13 11:09:53 -0700

    Anoma -> Alamo - xkwm1 fixed some more stuff

--- alamo.py
+++ alamo.py
@@ -6,10 +6,20 @@ import string
 import json
 import sys
 from requests.exceptions import ConnectionError
+import colorful as cf
 
 from colored import fore, back, style
 
 from colorama import Fore, Back, Style
+
+ci_colors = {
+    'mint': '#c5e8c8',  # RGB hex value
+    'darkRed': '#c11b55',  # RGB hex value
+    'lightBlue': (15, 138, 191),  # RGB channel triplet
+    'pink': '#FFC0CB'
+}
+
+cf.use_palette(ci_colors)
 
 chars = string.ascii_letters + string.digits + '!@#$%^&*()'
 random.seed = (os.urandom(1024))
@@ -19,10 +29,10 @@ init()
 from colorama import Fore, Back, Style
 
 import ctypes
-ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Menu")
+ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Menu")
 
 print(f"\n")
-print(f"{Fore.RED}[Anoma] {Fore.GREEN}Select a number from 1 - 6 to continue{Style.RESET_ALL}")
+print(f"{Fore.RED}[Alamo] {Fore.GREEN}Select a number from 1 - 6 to continue{Style.RESET_ALL}")
 print(f"[{Fore.GREEN}1{Style.RESET_ALL}] {Fore.CYAN}Start{Style.RESET_ALL}")
 print(f"[{Fore.GREEN}2{Style.RESET_ALL}] {Fore.CYAN}Help{Style.RESET_ALL}")
 print(f"[{Fore.GREEN}3{Style.RESET_ALL}] {Fore.CYAN}Settings{Style.RESET_ALL}")
@@ -34,14 +44,14 @@ optionInput = input("> ")
 if "1" in optionInput or "2" in optionInput or "3" in optionInput or "4" in optionInput or "5" in optionInput or "6" in optionInput:
     pass
 else:
-    print(f"{Fore.RED}[Anoma] {Fore.CYAN}Answer must be between 1 - 6")
+    print(f"[{Fore.RED}Alamo{Style.RESET_ALL}] {Fore.CYAN}Answer must be between 1 - 6")
     exit()
 
 if optionInput == "1":
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Configuring URL...")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring URL...")
 
-    print(Fore.RED + "[Anoma]" + Fore.CYAN + ' What is the scam URL?: ' + Style.RESET_ALL, end="")
+    print(Fore.RED + "[Alamo]" + Fore.CYAN + ' What is the scam URL?: ' + Style.RESET_ALL, end="")
     urlInput = input()
     url = 'https://'+urlInput
 
@@ -63,31 +73,31 @@ if optionInput == "1":
     # ==========================================================================================================
 
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Configuring Login...")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Login...")
 
-    print(Fore.RED + "[Anoma]" + Fore.CYAN + ' What is the login Form Data?: ' + Style.RESET_ALL, end="")
+    print(Fore.RED + "[Alamo]" + Fore.CYAN + ' What is the login Form Data?: ' + Style.RESET_ALL, end="")
     usernameInput = input()
 
 
     # ==========================================================================================================
 
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Configuring Password...")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Password...")
 
 
-    print(Fore.RED + "[Anoma]" + Fore.CYAN + ' What is the password Form Data?: ' + Style.RESET_ALL, end="")
+    print(Fore.RED + "[Alamo]" + Fore.CYAN + ' What is the password Form Data?: ' + Style.RESET_ALL, end="")
     passwordInput = input()
 
     # ==========================================================================================================
 
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Configuring Requests...")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Requests...")
 
 
     #requestInput = int(input(f"{Fore.RED}[SP] {Fore.CYAN}How many requests do you want to make?: {Style.RESET_ALL}"))
     #print(requestInput)
 
-    print(Fore.RED + "[Anoma]" + Fore.CYAN + ' How many request do you want to send?: ' + Style.RESET_ALL, end="")
+    print(Fore.RED + "[Alamo]" + Fore.CYAN + ' How many request do you want to send?: ' + Style.RESET_ALL, end="")
 
     request2Input = input()
 
@@ -107,10 +117,10 @@ if optionInput == "1":
     # ==========================================================================================================
 
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Configuring Proxy...")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Configuring Proxy...")
 
 
-    print(Fore.RED + "[Anoma]" + Fore.CYAN + ' Would you like to use a Proxy? (Choose "no" unless you want to be spammed with errors): ' + Style.RESET_ALL, end="")
+    print(Fore.RED + "[Alamo]" + Fore.CYAN + ' Would you like to use a Proxy? (Choose "no" unless you want to be spammed with errors): ' + Style.RESET_ALL, end="")
     proxyInput = input()
 
     if "yes" in proxyInput or "no" in proxyInput:
@@ -152,7 +162,7 @@ if optionInput == "1":
         	})
             print(f"Sending {style.BOLD}1{Style.RESET_ALL} requests with the Username {style.UNDERLINED}{username} and Password {style.UNDERLINED}{password}")
 
-        print(f"{Fore.RED}[Anoma] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
+        print(f"{Fore.RED}[Alamo] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
 
     else:
         name_extra = ''.join(random.choice(string.digits))
@@ -174,15 +184,15 @@ if optionInput == "1":
         	})
             print(f"Sending {style.BOLD}1{Style.RESET_ALL} requests with the Username {style.UNDERLINED}{username} and Password {style.UNDERLINED}{password}")
 
-        print(f"{Fore.RED}[Anoma] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
+        print(f"{Fore.RED}[Alamo] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
 
 
 if optionInput == "2":
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Help")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Help")
 
-    print(f"{Fore.RED}[Anoma] {Fore.GREEN}What do you need help with? Choose an option below:{Style.RESET_ALL}")
-    print(f"[{Fore.GREEN}1{Style.RESET_ALL}] {Fore.CYAN}Starting Anoma{Style.RESET_ALL}")
+    print(f"{Fore.RED}[Alamo] {Fore.GREEN}What do you need help with? Choose an option below:{Style.RESET_ALL}")
+    print(f"[{Fore.GREEN}1{Style.RESET_ALL}] {Fore.CYAN}Starting Alamo{Style.RESET_ALL}")
     print(f"[{Fore.GREEN}2{Style.RESET_ALL}] {Fore.CYAN}Settings{Style.RESET_ALL}")
     print(f"[{Fore.GREEN}3{Style.RESET_ALL}] {Fore.CYAN}Check{Style.RESET_ALL}")
     print(f"[{Fore.GREEN}4{Style.RESET_ALL}] {Fore.CYAN}Other{Style.RESET_ALL}")
@@ -191,7 +201,7 @@ if optionInput == "2":
     if "1" in helpInput or "2" in helpInput or "3" in helpInput or "4" in helpInput:
         pass
     else:
-        print(f"{Fore.RED}[Anoma] {Fore.CYAN}Answer must be between 1 - 4")
+        print(f"[{Fore.MAGENTA}Hydra{Style.RESET_ALL}] {Fore.CYAN}Answer must be between 1 - 4")
         exit()
 
     if helpInput == "1":
@@ -209,15 +219,15 @@ if optionInput == "2":
 
 if optionInput == "3":
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma ] | Settings")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo ] | Settings")
 
     print(f"Coming Soon...")
 
 if optionInput == "4":
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | Check")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | Check")
 
-    print(f'{Fore.RED}[Anoma] {Fore.CYAN}What website would you like to check? (Do not include "https://" in the URL.):', Style.RESET_ALL, end="")
+    print(f'{Fore.RED}[Alamo] {Fore.CYAN}What website would you like to check? (Do not include "https://" in the URL.):', Style.RESET_ALL, end="")
     checkInput = input()
 
     url = 'https://'+checkInput
@@ -233,13 +243,13 @@ if optionInput == "4":
         response = requests.get(url, allow_redirects=True, headers={'User-Agent': 'Chrome'})
         print(response.headers)
 
-    print(f"{Fore.RED}[Anoma] {Fore.CYAN}Would you like to see the code of {url}?{Style.RESET_ALL}")
+    print(f"{Fore.RED}[Alamo] {Fore.CYAN}Would you like to see the code of {url}?{Style.RESET_ALL}")
     codeInput = input("> ")
 
     if "yes" in codeInput or "no" in codeInput:
         pass
     else:
-        print(f"{Fore.RED}[Anoma] {Fore.CYAN}Answer must be either {Style.RESET_ALL}{style.BOLD}yes{Style.RESET_ALL} {Fore.CYAN}or {Style.RESET_ALL}{style.BOLD}no{Style.RESET_ALL}")
+        print(f"{Fore.RED}[Alamo] {Fore.CYAN}Answer must be either {Style.RESET_ALL}{style.BOLD}yes{Style.RESET_ALL} {Fore.CYAN}or {Style.RESET_ALL}{style.BOLD}no{Style.RESET_ALL}")
         # exit()
 
     if codeInput == 'yes':
@@ -249,15 +259,15 @@ if optionInput == "4":
         exit()
 
 if optionInput == "5":
-    print(f"[{Fore.GREEN}Name{Style.RESET_ALL}]: Anoma")
+    print(f"[{Fore.GREEN}Name{Style.RESET_ALL}]: Alamo")
     print(f"[{Fore.GREEN}Version{Style.RESET_ALL}]: 1.0.1")
     print(f"[{Fore.GREEN}Developer{Style.RESET_ALL}]: xKwm1")
-    print(f"[{Fore.GREEN}Licence{Style.RESET_ALL}]: Apache License 2.0 (Read here: https://github.com/xkwm1/Anoma/blob/main/LICENSE)")
+    print(f"[{Fore.GREEN}Licence{Style.RESET_ALL}]: Apache License 2.0 (Read here: https://github.com/xkwm1/Alamo/blob/main/LICENSE)")
     print(f"[{Fore.RED}Website{Style.RESET_ALL}]: Coming Soon{Style.RESET_ALL}")
     print(f"[{Fore.RED}Discord{Style.RESET_ALL}]: Coming Soon{Style.RESET_ALL}")
-    print(f"[{Fore.RED}GitHub{Style.RESET_ALL}]: https://github.com/xkwm1/Anoma{Style.RESET_ALL}")
+    print(f"[{Fore.RED}GitHub{Style.RESET_ALL}]: https://github.com/xkwm1/Alamo{Style.RESET_ALL}")
 
     import ctypes
-    ctypes.windll.kernel32.SetConsoleTitleW("[Anoma 1.0.1] | About")
+    ctypes.windll.kernel32.SetConsoleTitleW("[Alamo 1.0.1] | About")
 if optionInput == "6":
     exit();

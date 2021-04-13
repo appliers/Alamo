8b07c10a858adb2b82f67ef33865f18c296fa584
xkwm1 <xkwm1yt@gmail.com>
2021-04-12 18:50:27 -0700

    fixed some stuff

--- scams.py
+++ scams.py
@@ -23,11 +23,16 @@ print(Fore.RED + "[SP]" + Fore.CYAN + ' What is the scam URL?: ' + Style.RESET_A
 urlInput = input()
 url = 'https://'+urlInput
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+#request = requests.get(f"{url}")
+#if request.status_code == 200:
+#    pass
+#else:
+#    print(f'{url} is not a valid URL. Please try a different URL.')
 
-request = requests.get(url, headers=headers)
-if request.status_code == 400:
-    print('This website does not exist bruh')
+response = requests.get(f'{url}')
+assert response.status_code < 400:
+    print('no')
+
 
 # ==========================================================================================================
 
@@ -88,33 +93,45 @@ if proxyInput == "yes":
         "https" : https_proxy,
         }
 
+    name_extra = ''.join(random.choice(string.digits))
+
+    for i in range(requestInput):
+        for i in range(random.randint(19,19)):
+
+            chars = list(string.ascii_lowercase)+list(string.ascii_uppercase)+list(string.digits)
+            password = ""
+            password = ''.join(random.choice(chars) for i in range(8)) # Or whatever amount of requests you wish to send
+            name_extra = ''.join(random.choice(string.digits))
+            username = 'kingsman' + name_extra + '@yahoo.com'
+
+        # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
+
+        requests.post(url, allow_redirects=False, headers={'User-Agent': 'Chrome'}, proxies=proxyDict, data={
+        	f"{usernameInput}": username,
+        	f"{passwordInput}": password
+    	})
+        print(f"Sending {style.BOLD}1{Style.RESET_ALL} requests with the Username {style.UNDERLINED}{username} and Password {style.UNDERLINED}{password}")
+
+    print(f"{Fore.RED}[SP] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
+
 else:
-        http_proxy  = "http://127.0.0.1"
-        https_proxy = "https://127.0.0.1"
+    name_extra = ''.join(random.choice(string.digits))
 
+    for i in range(requestInput):
+        for i in range(random.randint(19,19)):
 
-        proxyDict = {
-            "http"  : http_proxy,
-            "https" : https_proxy,
-            }
+            chars = list(string.ascii_lowercase)+list(string.ascii_uppercase)+list(string.digits)
+            password = ""
+            password = ''.join(random.choice(chars) for i in range(8)) # Or whatever amount of requests you wish to send
+            name_extra = ''.join(random.choice(string.digits))
+            username = 'kingsman' + name_extra + '@yahoo.com'
 
-name_extra = ''.join(random.choice(string.digits))
+        # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
 
-for i in range(requestInput):
-    for i in range(random.randint(19,19)):
+        requests.post(url, allow_redirects=False, headers={'User-Agent': 'Chrome'}, data={
+    		f"{usernameInput}": username,
+    		f"{passwordInput}": password
+    	})
+        print(f"Sending {style.BOLD}1{Style.RESET_ALL} requests with the Username {style.UNDERLINED}{username} and Password {style.UNDERLINED}{password}")
 
-        chars = list(string.ascii_lowercase)+list(string.ascii_uppercase)+list(string.digits)
-        password = ""
-        password = ''.join(random.choice(chars) for i in range(8)) # Or whatever amount of requests you wish to send
-        name_extra = ''.join(random.choice(string.digits))
-        username = 'kingsman' + name_extra + '@yahoo.com'
-
-    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'}
-
-    requests.post(url, allow_redirects=False, headers={'User-Agent': 'Chrome'}, proxies=proxyDict, data={
-		f"{usernameInput}": username,
-		f"{passwordInput}": password
-	})
-    print(f"Sending {style.BOLD}1{Style.RESET_ALL} requests with the Username {style.UNDERLINED}{username} and Password {style.UNDERLINED}{password}")
-
-print(f"{Fore.RED}[SP] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
+    print(f"{Fore.RED}[SP] {Fore.CYAN}Successfully sent {Style.RESET_ALL}{style.BOLD}{requestInput}{Style.RESET_ALL}{Fore.CYAN} requests to {style.UNDERLINED}{url}.")
